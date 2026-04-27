23273f2dcdb3254b022c5768c318a9c7518f596c
ScandalousPigeon <951413eelnoel@gmail.com>
2026-04-27 18:49:24 +1000

    Styled in-progress card, added resizing to HelloApplication.

--- src/main/java/com/example/cab302_project/HelloApplication.java
+++ src/main/java/com/example/cab302_project/HelloApplication.java
@@ -14,6 +14,9 @@ public class HelloApplication extends Application {
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("Hello!");
         stage.setScene(scene);
+        stage.setResizable(true);
+        stage.setMinWidth(1200);
+        stage.setMinHeight(900);
         stage.show();
     }
 }
