bd1d93619750bc2bdd1b65782bb9c1d54cc503d3
ScandalousPigeon <951413eelnoel@gmail.com>
2026-04-28 22:20:26 +1000

    Sign in and sign up links now change to their respective screens

--- src/main/java/com/example/cab302_project/controller/LoginScreenController.java
+++ src/main/java/com/example/cab302_project/controller/LoginScreenController.java
@@ -1,4 +1,39 @@
 package com.example.cab302_project.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.control.Button;
+import javafx.scene.control.Hyperlink;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class LoginScreenController {
+    @FXML
+    private Hyperlink signUpButton;
+    @FXML
+    private Button logInButton;
+
+    @FXML
+    private void handleLogIn(ActionEvent event) throws IOException {
+        switchScreen(event, "/com/example/cab302_project/main-menu.fxml");
+    }
+
+    @FXML
+    private void handleSignUp(ActionEvent event) throws IOException {
+        switchScreen(event, "/com/example/cab302_project/sign-up.fxml");
+    }
+
+    private void switchScreen(ActionEvent event, String fxmlPath) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource(fxmlPath));
+
+        Stage stage = (Stage) ((Node) event.getSource())
+                .getScene()
+                .getWindow();
+
+        stage.getScene().setRoot(root);
+    }
 }
