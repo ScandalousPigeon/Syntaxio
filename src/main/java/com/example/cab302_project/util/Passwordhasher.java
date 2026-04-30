a9e4c76d7db90fc0789e6abf44ed11309f0671eb
Mimi Owor <eno.owor@icloud.com>
2026-04-30 17:13:48 +1000

    passwordhash and database files (unfinished)

---
+++ src/main/java/com/example/cab302_project/util/Passwordhasher.java
@@ -0,0 +1,25 @@
+package com.example.cab302_project.util;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
+
+public class Passwordhasher {
+    
+    public static String hashPassword(String password) {
+        try {
+            MessageDigest digest = MessageDigest.getInstance("SHA-256");
+            byte[] hash = digest.digest(password.getBytes());
+            return Base64.getEncoder().encodeToString(hash);
+
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException("SHA-256 algorithm not found", e);
+        }
+    }
+
+    public static boolean verifyPassword(String plainPassword, String hashedPassword) {
+        String hashedInput = hashPassword(plainPassword);
+        return hashedInput.equals(hashedPassword);
+    }
+
+}
